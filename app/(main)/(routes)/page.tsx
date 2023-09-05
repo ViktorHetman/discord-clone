0bbfd7eb1699de9536866c43cae6ca05fc651187
Viktor <vitya.getman.79@gmail.com>
2023-09-05 13:28:30 +0200

    theme toggle

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
 
+import { ModeToggle } from "@/components/ModeToggle";
+
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
