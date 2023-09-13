ae8dad6d59692398e73c0c7706348bdcae2ed505
Viktor <vitya.getman.79@gmail.com>
2023-09-13 15:21:25 +0200

    modal for creating new servers + zustand + managing modals via custom hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+
 import { cn } from "@/lib/utils";
+
 import "./globals.css";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -28,6 +31,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
