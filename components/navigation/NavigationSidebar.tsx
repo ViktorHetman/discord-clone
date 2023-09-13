c3078c86ca3fb80a6f9802171819f3a0f56ff3a9
Viktor <vitya.getman.79@gmail.com>
2023-09-13 14:56:26 +0200

    finishing sidebar navigation

--- components/navigation/NavigationSidebar.tsx
+++ components/navigation/NavigationSidebar.tsx
@@ -1,6 +1,12 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 import NavigationAction from "./NavigationAction";
+import NavigationItem from "./NavigationItem";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ModeToggle } from "@/components/ModeToggle";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
@@ -23,6 +29,30 @@ const NavigationSidebar = async () => {
   return (
     <nav className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              avatarBox: "h-[48px] w-[48px]",
+            },
+          }}
+        />
+      </div>
     </nav>
   );
 };
