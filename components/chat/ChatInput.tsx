d1e8e1036db45d1c05b0000d0667fc97fe478328
Viktor <vitya.getman.79@gmail.com>
2023-10-22 18:20:12 +0200

    emoji picker

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import * as zod from "zod";
 import axios from "axios";
 import qs from "query-string";
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import EmojiPicker from "@/components/EmojiPicker";
 
 import { ChatInputProps } from "@/types/propsTypes";
 import { validationForChatInput } from "@/lib/formValidation";
@@ -16,6 +18,7 @@ import { useModal } from "@/hooks/useModalStore";
 
 const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
   const { onOpen } = useModal();
+  const router = useRouter();
 
   const form = useForm<zod.infer<typeof validationForChatInput>>({
     defaultValues: {
@@ -36,6 +39,8 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
       });
 
       await axios.post(url, values);
+      form.reset();
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
@@ -53,15 +58,12 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    onClick={() => {}}
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 
                     hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex
                     items-center justify-center"
                   >
-                    <Plus
-                      className="text-white dark:text-[#313338]"
-                      onClick={() => onOpen("messageFile", { apiUrl, query })}
-                    />
+                    <Plus className="text-white dark:text-[#313338]" />
                   </button>
                   <Input
                     disabled={isLoading}
@@ -73,7 +75,11 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     {...field}
                   />
                   <div className="absolute top-7 right-8">
-                    <Smile />
+                    <EmojiPicker
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value}${emoji}`)
+                      }
+                    />
                   </div>
                 </div>
               </FormControl>
